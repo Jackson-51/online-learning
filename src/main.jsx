5b5e5b90733edb7105b1cad98eecea88673e1191
Ojo Tolulope <toluene1123@gmail.com>
2025-12-11 21:43:33 +0100

    feat: Made the Header and installed Shadcn

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './pages/App.jsx'
+import AppLayout from './components/universal/AppLayout.jsx'
 import { createBrowserRouter, RouterProvider} from 'react-router'
+import Requests from './pages/Requests.jsx'
 
 const router = createBrowserRouter([
-  {path: "/", element: <App />},
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <p>Error page</p>,
+    children: [
+      { index: true, element: <App /> },
 
+      {
+        path: "requests",
+        element: < Requests/>,
+        // loader: productsLoader,       // LISTING LOADER
+      },
+
+      // {
+      //   path: "products/new",
+      //   element: <NewProductPage />,
+      //   action: newProductAction,     // CREATE ACTION
+      // },
+    ],
+  },
 ]);
 
 createRoot(document.getElementById('root')).render(
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      
+      I am a boy
     </div>
   );
 }
